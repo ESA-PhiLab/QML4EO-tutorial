fa3685f2e75cc63f63f473cbaca20c1dc1a6b601
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-02-14 09:32:22 +0100

    commenting code

--- models/HybridNet.py
+++ models/HybridNet.py
@@ -6,16 +6,44 @@ import torch
 from config import *
 
 class HybridNet(nn.Module):
+    '''
+        Hybrid Quantum Convolutional Neural Network (PyTorch - Qiskit)
+    '''
+
     def __init__(self):
+        '''
+            HybridNet constructor
+
+            Parameters
+            ----------
+            Nothing
+
+            Returns
+            -------
+            Nothing, a HybridNet object is created
+        '''
+
         super(HybridNet, self).__init__()
         self.conv1 = nn.Conv2d(3,  16, kernel_size=3)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
         self.fc1 = nn.Linear(2304, NUM_LAYERS * NUM_QUBITS)
-        self.qc = TorchCircuit.apply
+        self.qc = TorchCircuit.apply # Quantum Layer
         self.fc2 = nn.Linear(NUM_QC_OUTPUTS, CLASSES)
 
     def forward(self, x):
+        '''
+            Forward function of a PyTorch model
+
+            Parameters
+            ----------
+            - x: tensor representing an image
+
+            Returns
+            -------
+            - x: tensor representing model output
+        
+        '''
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = F.relu(F.max_pool2d(self.conv3(x), 2))
@@ -29,6 +57,17 @@ class HybridNet(nn.Module):
         return x
 
     def predict(self, x):
+        '''
+            Predict output using model (during training must be runned into model.evaluate or with torch.nograd)
+            
+            Parameters
+            ----------
+            - x: tensor representing an image
+
+            Returns
+            -------
+            - ans: tensor representing model output
+        '''
         pred = self.forward(x)
         ans = torch.argmax(pred[0]).item()
         return torch.tensor(ans)
