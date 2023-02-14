1550ceb3483b80e385bddd5d0353c1a2dd500ccd
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-02-14 11:44:24 +0100

    confing for tutorial (timing)

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import qiskit
 import os
 
 #----------------------- Quantum Circuit Settings -----------------------
-NUM_QUBITS      = 6
-NUM_SHOTS       = 100
-NUM_LAYERS      = 3
+NUM_QUBITS      = 4
+NUM_SHOTS       = 1 # for timing reasons is set to 1, but in IRL you want this value to be higher https://quantumcomputing.stackexchange.com/questions/9823/what-is-meant-with-shot-in-quantum-computation
+NUM_LAYERS      = 2
 SHIFT           = np.pi/4
 
 def create_QC_OUTPUTS():
@@ -21,7 +21,6 @@ SIMULATOR       = qiskit.Aer.get_backend('qasm_simulator')
 #----------------------- Dataset Settings -----------------------
 DATASET_ROOT    = os.path.join('content','EuroSAT')
 SPLIT_FACTOR    = 0.2
-
 
 CLASS_DICT      = {
     "AnnualCrop":           0,
