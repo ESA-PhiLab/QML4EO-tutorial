c89123e783f847c4ca3633f0355ff17cfcd113d7
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-05-31 15:27:36 +0200

    new conf

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 #----------------------- Quantum Circuit Settings -----------------------
 NUM_QUBITS      = 4
 NUM_SHOTS       = 1 # for timing reasons is set to 1, but in IRL you want this value to be higher https://quantumcomputing.stackexchange.com/questions/9823/what-is-meant-with-shot-in-quantum-computation
-NUM_LAYERS      = 2
+NUM_LAYERS      = 5
 SHIFT           = np.pi/4
 
 def create_QC_OUTPUTS():
